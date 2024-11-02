e2269b07881897bc6053b00e2d9692c7c320815c
Rayhan <44944844+ToxicBeastt@users.noreply.github.com>
2024-11-02 08:09:59 +0700

    init commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
+import { useFonts } from 'expo-font'
 import 'react-native-reanimated';
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {Stack} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded, error] = useFonts({
+    'SpaceMono-Regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    if(error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!fontsLoaded && !error) return null
+
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack initialRouteName="Login">
+        <Stack.Screen name="index" />
       </Stack>
-    </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Welcome to the Home Screen!</Text>
+            <Button title="Go to Profile" />
+        </View>
+    );
+};
+
+export default App;
